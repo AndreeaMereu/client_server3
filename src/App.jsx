bfa9cffaf64795315a53aea1ed6b54a1c504ef32
valentin.cocorada <valentin.cocorada@metro.digital>
2023-12-02 21:32:51 +0200

    am adus la starea initiala componenta de loading

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,11 @@ function App() {
       <div className="lds-roller">
         <div></div>
         <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
       </div>
     );
   }
